6bfebbe58bdacf0e4bbedb56018bc308cb196250
emsmariee <751990@student.dusd.net>
2022-12-15 11:43:37 -0800

    Imported downloads. created background
    imported downloads from canvas. made game background, started scripts

---
+++ FlappyBirdFinalZunigaP3/Assets/Scripts/Bird.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce;
+    private bool isDead = false;
+
+    private Animator anim;
+    private Rigidbody rb2d;
+  
+    void Start()
+    {
+        //Get refernce to the animator componment attached to this game object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
